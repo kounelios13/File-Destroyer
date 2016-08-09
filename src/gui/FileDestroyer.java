58b3c838d6d03367f17f56cceae7330f2d717e3a
kounelios13 <pkounelios@gmail.com>
2016-08-09 14:31:44 +0300

    first commit

---
+++ src/gui/FileDestroyer.java
@@ -0,0 +1,13 @@
+package gui;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+@SuppressWarnings({"serial","unused"})
+public class FileDestroyer extends JFrame{
+
+	private int selectedIndex = -1;
+	private JPanel panel = new JPanel();
+	private void initUI(){
+		
+	}
+}
