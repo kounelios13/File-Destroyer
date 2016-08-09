c6b8586badd667d25d07a65f65b369266a61d07c
kounelios13 <pkounelios@gmail.com>
2016-08-09 16:22:07 +0300

    not deletting folders

--- src/gui/FileDestroyer.java
+++ src/gui/FileDestroyer.java
@@ -2,13 +2,17 @@ package gui;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.stream.Stream;
 
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
+
+import utils.FileDrop;
 @SuppressWarnings({"serial","unused"})
 public class FileDestroyer extends JFrame{
 
@@ -20,11 +24,40 @@ public class FileDestroyer extends JFrame{
 	private void initUI(){
 		JButton deleteSelected = new JButton("Delete selected"),
 				deleteAll = new JButton("Delete all");
-		
-	
+		panel.add(list);
+		panel.add(deleteSelected);
+		panel.add(deleteAll);
+		deleteSelected.addActionListener((e)->{
+			if(selectedIndex==-1)
+				return;
+			boolean death = criminals.get(selectedIndex).delete();
+			if(death)
+				JOptionPane.showMessageDialog(null,"File deleted");
+			else
+				JOptionPane.showMessageDialog(null, "File not deleted");
+			if(death){
+				model.removeElementAt(selectedIndex);
+				criminals.remove(selectedIndex);
+			}
+		});
+		deleteAll.addActionListener((e)->{
+			for(File f:criminals)
+				f.delete();
+		});
+		setContentPane(panel);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		setSize(800,600);
+		this.setLocationRelativeTo(null);
+		setVisible(true);
 	}
 	public FileDestroyer(){
 		initUI();
+		new FileDrop(this,(e)->{
+			for(File f:e){
+				criminals.add(f);
+				model.addElement(f.getName()+(f.isDirectory()?" (Folder)":""));
+			}
+		});
 	}
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(()->new FileDestroyer());
