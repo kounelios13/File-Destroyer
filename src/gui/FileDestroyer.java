b45d4e7f30346df54aa8939952843c9820a18f8f
kounelios13 <pkounelios@gmail.com>
2016-08-09 14:42:04 +0300

    added buttons

--- src/gui/FileDestroyer.java
+++ src/gui/FileDestroyer.java
@@ -1,15 +1,27 @@
 package gui;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.SwingUtilities;
 @SuppressWarnings({"serial","unused"})
 public class FileDestroyer extends JFrame{
 
+	private ArrayList<File> criminals = new ArrayList<>();
+	private DefaultComboBoxModel<String> model = new DefaultComboBoxModel<>();
+	private JComboBox<String> list = new JComboBox<>(model);
 	private int selectedIndex = -1;
 	private JPanel panel = new JPanel();
 	private void initUI(){
+		JButton deleteSelected = new JButton("Delete selected"),
+				deleteAll = new JButton("Delete all");
 		
+	
 	}
 	public FileDestroyer(){
 		initUI();
